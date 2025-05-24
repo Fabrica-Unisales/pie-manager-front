8add2ba6c55bb70b3518e5aea70cb12d58b63734
lucasguimaraes2005 <lucaagui123@gmail.com>
2025-05-24 10:49:33 -0300

    feat: criar estrutura inicial da página de avaliações

--- src/app/(auth)/avaliacoes/page.js
+++ src/app/(auth)/avaliacoes/page.js
@@ -5,11 +5,40 @@ import { columns } from '@/statics/tableMocks';
 
 
 const App = () => {
+    const handleAddItem = () => {
+      window.location.href = '/avaliacoes/new';
+    };
+  
+    const [data, setData] = useState([]);
+    useEffect(() => {
+      const items = JSON.parse(localStorage.getItem('avaliacoes')) || [];
+      if (items.length > 0) {
+        setData(items.data);
+      }
+    }, []);
+  
     return (
-        <div>
-            <h1>Avaliações</h1>
+      <div style={{ padding: 24 }}>
+        <div style={{ marginBottom: 16, textAlign: 'right' }}>
+          <Button
+            type="primary"
+            style={{
+              background: '#1890ff',
+              color: '#fff',
+              border: 'none',
+              borderRadius: 4,
+              padding: '8px 16px',
+              cursor: 'pointer',
+              fontSize: 16,
+            }}
+            onClick={handleAddItem}
+          >
+            Adicionar Avaliação
+          </Button>
         </div>
+        <Table columns={columns} dataSource={data} />
+      </div>
     );
-};
-
-export default App;
+  }
+  
+  export default App;
