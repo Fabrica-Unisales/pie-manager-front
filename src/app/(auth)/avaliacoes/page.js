f1b41f045540ae4bde5c7978b797e13724b0261a
lucasguimaraes2005 <lucaagui123@gmail.com>
2025-05-24 13:23:44 -0300

    chore: remover import não utilizado 'use' em AvaliacoesPage

--- src/app/(auth)/avaliacoes/page.js
+++ src/app/(auth)/avaliacoes/page.js
@@ -1,5 +1,5 @@
 'use client';
-import React, { use, useEffect, useState } from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Table } from 'antd';
 import { columns } from '@/statics/tableMocks';
 
