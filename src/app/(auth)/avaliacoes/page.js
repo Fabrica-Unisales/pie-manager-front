95053d6d21c2f556f2da4004470a83b1b6df9aa0
lucasguimaraes2005 <lucaagui123@gmail.com>
2025-05-24 10:43:20 -0300

    chore: reorganizar pastas para corrigir estrutura do projeto

---
+++ src/app/(auth)/avaliacoes/page.js
@@ -0,0 +1,15 @@
+'use client';
+import React, { use, useEffect, useState } from 'react';
+import { Button, Table } from 'antd';
+import { columns } from '@/statics/tableMocks';
+
+
+const App = () => {
+    return (
+        <div>
+            <h1>Avaliações</h1>
+        </div>
+    );
+};
+
+export default App;
