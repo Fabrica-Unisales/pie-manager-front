e4bebb81d5f09dda0fb33603179e4eb18776006d
Luisarabelo <lsrabelovix@gmail.com>
2025-06-15 19:16:59 -0300

    Ajustei o layout para que a barra de menu continuasse aparecendo mesmo após clicar na tela de controle.

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,23 +2,59 @@
 "use client";
 import React from "react";
 import { AntdRegistry } from "@ant-design/nextjs-registry";
-import { Breadcrumb, Layout, Menu, theme } from "antd";
+import { Layout, Menu, theme } from "antd";
 import { items2 } from "@/statics/menuItens";
+import "./globals.css";
 
 const { Header, Content, Sider } = Layout;
 
-const App = ({children}) => {
+const RootLayout = ({ children }) => {
   const {
-    token: { colorBgContainer, borderRadiusLG },
+    token: { colorBgContainer },
   } = theme.useToken();
+
   return (
-    <html lang="en">
+    <html lang="pt-BR">
       <body>
         <AntdRegistry>
+          <Layout style={{ minHeight: "100vh" }}>
+            <Sider
+              collapsible
+              style={{ position: 'fixed', height: '100vh', left: 0, top: 0, bottom: 0 }}
+            >
+              <div style={{
+                height: 32,
+                margin: 16,
+                background: "rgba(255, 255, 255, 0.3)",
+                borderRadius: 6
+              }} />
+              <Menu
+                theme="dark"
+                mode="inline"
+                defaultSelectedKeys={["home"]}
+                items={items2}
+              />
+            </Sider>
+
+            <Layout style={{ marginLeft: 200 }}> {/* margem para compensar o Sider fixo */}
+              <Header style={{ padding: 0, background: colorBgContainer }} />
+              <Content style={{ margin: '16px' }}>
+                <div
+                  style={{
+                    padding: 24,
+                    minHeight: 360,
+                    background: colorBgContainer,
+                  }}
+                >
                   {children}
+                </div>
+              </Content>
+            </Layout>
+          </Layout>
         </AntdRegistry>
       </body>
     </html>
   );
 };
-export default App;
+
+export default RootLayout;
