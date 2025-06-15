747bccb4bdc0c39d922f82374212c891fe049c2b
Beatriz-Stein <Btrzstein5@gmail.com>
2025-06-15 13:32:10 -0300

    mocks do usuario

--- src/mocks/UserMocks.js
+++ src/mocks/UserMocks.js
@@ -1,11 +1,25 @@
-export default class UserMocks{
+export default class UserMocks {
     static build() {
-        const users = [
-            { username: 'admin@admin', password: 'admin' },
-            { username: 'user2', password: 'password2' },
-            { username: 'user3', password: 'password3' },
-        ];
+        const users = {
+            data: [
+                { username: 'admin@admin', password: 'admin' },
+                { username: 'user2', password: 'password2' },
+                { username: 'user3', password: 'password3' },
 
+                { id: 'p-ranya', nome: 'Prof Ranya Castro', username: 'ranya@prof', password: '123', tipo: 'Professor' },
+                { id: 'p-kelly', nome: 'Prof Kelly Soares', username: 'kelly@prof', password: '123', tipo: 'Professor' },
+                { id: 'p-marciano', nome: 'Prof Marciano Luz', username: 'marciano@prof', password: '123', tipo: 'Professor' },
+                { id: 'p-samira', nome: 'Prof Samira Dias', username: 'samira@prof', password: '123', tipo: 'Professor' },
+                { id: 'p-heitor', nome: 'Prof Heitor Ramos', username: 'heitor@prof', password: '123', tipo: 'Professor' },
+                { id: 'p-larissa', nome: 'Prof Larissa Melo', username: 'larissa@prof', password: '123', tipo: 'Professor' },
+
+                { id: 'aE-fernando', nome: 'Avaliador Fernando Dias', username: 'fernando@avaliador', password: '123', tipo: 'AvaliadorExterno' },
+                { id: 'aE-cintia', nome: 'Avaliadora Cíntia Lopes', username: 'cintia@avaliador', password: '123', tipo: 'AvaliadorExterno' },
+                { id: 'aE-leonardo', nome: 'Avaliador Leonardo Faria', username: 'leonardo@avaliador', password: '123', tipo: 'AvaliadorExterno' }
+            ],
+            nextId: 27, 
+            length: 26
+        };
         localStorage.setItem('users', JSON.stringify(users));
     }
 }
