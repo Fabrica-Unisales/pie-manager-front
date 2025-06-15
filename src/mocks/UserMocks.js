74195496f8ba22447e30541b06f96677f89a347a
Beatriz-Stein <Btrzstein5@gmail.com>
2025-06-15 15:20:41 -0300

    correções

--- src/mocks/UserMocks.js
+++ src/mocks/UserMocks.js
@@ -1,78 +1,23 @@
-export default class UserMocks{
+export default class UserMocks {
     static build() {
-        const users = {
-            data: [
-                { username: 'admin@admin', password: 'admin' },
-                { username: 'user2', password: 'password2' },
-                { username: 'user3', password: 'password3' },
-
-                {
-                    id: 'p-ranya',
-                    nome: 'Prof Ranya Castro',
-                    username: 'ranya@prof',
-                    password: '123',
-                    tipo: 'Professor'
-                },
-                {
-                    id: 'p-kelly',
-                    nome: 'Prof Kelly Soares',
-                    username: 'kelly@prof',
-                    password: '123',
-                    tipo: 'Professor'
-                },
-                {
-                    id: 'p-marciano',
-                    nome: 'Prof Marciano Luz',
-                    username: 'marciano@prof',
-                    password: '123',
-                    tipo: 'Professor'
-                },
-                {
-                    id: 'p-samira',
-                    nome: 'Prof Samira Dias',
-                    username: 'samira@prof',
-                    password: '123',
-                    tipo: 'Professor'
-                },
-                {
-                    id: 'p-heitor',
-                    nome: 'Prof Heitor Ramos',
-                    username: 'heitor@prof',
-                    password: '123',
-                    tipo: 'Professor'
-                },
-                {
-                    id: 'p-larissa',
-                    nome: 'Prof Larissa Melo',
-                    username: 'larissa@prof',
-                    password: '123',
-                    tipo: 'Professor'
-                },
-                {
-                    id: 'aE-fernando',
-                    nome: 'Avaliador Fernando Dias',
-                    username: 'fernando@avaliador',
-                    password: '123',
-                    tipo: 'AvaliadorExterno'
-                },
-                {
-                    id: 'aE-cintia',
-                    nome: 'Avaliadora Cíntia Lopes',
-                    username: 'cintia@avaliador',
-                    password: '123',
-                    tipo: 'AvaliadorExterno'
-                },
-                {
-                    id: 'aE-leonardo',
-                    nome: 'Avaliador Leonardo Faria',
-                    username: 'leonardo@avaliador',
-                    password: '123',
-                    tipo: 'AvaliadorExterno'
-                }   
-            ],
-            nextId: 27, 
-            length: 26
-        };
-        localStorage.setItem('users', JSON.stringify(users));
+      const data = [
+        { key: '1', username: 'admin@admin', password: 'admin' },
+        { key: '2', username: 'user2', password: 'password2' },
+        { key: '3', username: 'user3', password: 'password3' },
+  
+        { key: '4', id: 'p-ranya', nome: 'Professora Ranya Castro', username: 'ranya@prof', password: '123', tipo: 'Professor' },
+        { key: '5', id: 'p-kelly', nome: 'Professora Kelly Soares', username: 'kelly@prof', password: '123', tipo: 'Professor' },
+        { key: '6', id: 'p-marciano', nome: 'Professor Marciano Luz', username: 'marciano@prof', password: '123', tipo: 'Professor' },
+        { key: '7', id: 'p-samira', nome: 'Professora Samira Dias', username: 'samira@prof', password: '123', tipo: 'Professor' },
+        { key: '8', id: 'p-heitor', nome: 'Professor Heitor Ramos', username: 'heitor@prof', password: '123', tipo: 'Professor' },
+        { key: '9', id: 'p-larissa', nome: 'Professora Larissa Melo', username: 'larissa@prof', password: '123', tipo: 'Professor' },
+  
+        { key: '10', id: 'aE-fernando', nome: 'Avaliador Fernando Dias', username: 'fernando@avaliador', password: '123', tipo: 'AvaliadorExterno' },
+        { key: '11', id: 'aE-cintia', nome: 'Avaliadora Cíntia Lopes', username: 'cintia@avaliador', password: '123', tipo: 'AvaliadorExterno' },
+        { key: '12', id: 'aE-leonardo', nome: 'Avaliador Leonardo Faria', username: 'leonardo@avaliador', password: '123', tipo: 'AvaliadorExterno' }
+      ];
+  
+      localStorage.setItem('users', JSON.stringify(data));
     }
-}
+  }
+  
