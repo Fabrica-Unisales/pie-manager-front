4aa54de9d49a3ccb14f5bf3a689ab4dcf828ff77
Euclides Silva Neto <euclidespu21@gmail.com>
2025-06-16 21:56:07 -0300

    Corrige menuItens para resolver conflito e ajustar navegação

--- src/statics/menuItens.js
+++ src/statics/menuItens.js
@@ -16,15 +16,13 @@ export const items2 = [
     label: <a href="/itens" rel="itens">Itens</a>,
   },
   {
-{
-  key: "users",
-  icon: React.createElement(TeamOutlined),
-  label: <a href="/users" rel="users">Controle de Usuários</a>,
-},
-{
-  key: "avaliacao",
-  icon: React.createElement(NotificationOutlined), 
-  label: <a href="/controleAvaliacoes" rel="controle-avaliacoes">Controle Avaliação</a>, 
-},
+    key: "users",
+    icon: React.createElement(TeamOutlined),
+    label: <a href="/users" rel="users">Controle de Usuários</a>,
+  },
+  {
+    key: "avaliacao",
+    icon: React.createElement(NotificationOutlined),
+    label: <a href="/controleAvaliacoes" rel="controle-avaliacoes">Controle Avaliação</a>,
   },
 ];
