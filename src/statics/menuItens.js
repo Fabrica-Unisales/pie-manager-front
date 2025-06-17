20a6fac90809e20d8d5d6cadd697cedf765e4391
blink992 <alleybossmangas@gmail.com>
2025-06-17 20:15:35 -0300

    att

--- src/statics/menuItens.js
+++ src/statics/menuItens.js
@@ -1,18 +1,33 @@
 "use client";
 import React from "react";
-import {
-  NotificationOutlined,
-} from "@ant-design/icons";
+import { NotificationOutlined } from "@ant-design/icons";
 
 export const items2 = [
   {
     key: "home",
     icon: React.createElement(NotificationOutlined),
-    label: <a href="/home" rel="home">Home</a>,
+    label: (
+      <a href="/home" rel="home">
+        Home
+      </a>
+    ),
   },
   {
     key: "send",
     icon: React.createElement(NotificationOutlined),
-    label: <a href="/itens" rel="itens">Itens</a>,
+    label: (
+      <a href="/itens" rel="itens">
+        Itens
+      </a>
+    ),
+  },
+  {
+    key: "usuario",
+    icon: React.createElement(NotificationOutlined),
+    label: (
+      <a href="/usuario" rel="usuario">
+        Cadastrar usuário
+      </a>
+    ),
   },
 ];
